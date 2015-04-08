5256c74b3e66ab02a74f92405ba73c44afa98172
Richard Parris <github@hda0.net>
2015-04-08 13:08:38 +0800

    Create choosebrowser.py

---
+++ choosebrowser.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import sys
+import webbrowser
+import subprocess
+from gi.repository import Gtk
+
+class ButtonWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Button Demo")
+        self.set_border_width(10)
+
+        hbox = Gtk.Box(spacing=6)
+        self.add(hbox)
+
+        button = Gtk.Button("Firefox")
+        button.connect("clicked", self.on_ff_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button("Chrome")
+        button.connect("clicked", self.on_cr_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button("_Close", use_underline=True)
+        button.connect("clicked", self.on_close_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+    def on_ff_clicked(self, button):
+        webbrowser.get('firefox').open_new_tab(sys.argv[1])
+        Gtk.main_quit()
+
+    def on_cr_clicked(self, button):
+        #webbrowser.get('chromium').open_new_tab(sys.argv[1])
+        process_one = subprocess.Popen(['google-chrome-stable', sys.argv[1]])
+        Gtk.main_quit()
+
+    def on_close_clicked(self, button):
+        print("Closing application")
+        Gtk.main_quit()
+
+win = ButtonWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
+
