17cb40af20651744c546663b29ec6f74f0c48e89
arpee <arpee@hda0.net>
2015-09-16 09:19:45 +0800

    added chromium to options

--- choosebrowser.py
+++ choosebrowser.py
@@ -8,21 +8,29 @@ class ButtonWindow(Gtk.Window):
 
     def __init__(self):
         Gtk.Window.__init__(self, title="Choose Browser")
-        self.set_border_width(20)
+        self.set_border_width(120)
 
-        hbox = Gtk.Box(spacing=6)
+        hbox = Gtk.Box(spacing=20)
         self.add(hbox)
 
         button = Gtk.Button("Firefox")
         button.connect("clicked", self.on_ff_clicked)
+        #button.set_border_width(40)
         hbox.pack_start(button, True, True, 0)
 
         button = Gtk.Button("Chrome")
         button.connect("clicked", self.on_cr_clicked)
+        #button.set_border_width(40)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button("Chromium")
+        button.connect("clicked", self.on_cr_clicked)
+        #button.set_border_width(40)
         hbox.pack_start(button, True, True, 0)
 
         button = Gtk.Button("_Close", use_underline=True)
         button.connect("clicked", self.on_close_clicked)
+        #button.set_border_width(40)
         hbox.pack_start(button, True, True, 0)
 
     def on_ff_clicked(self, button):
@@ -32,6 +40,10 @@ class ButtonWindow(Gtk.Window):
     def on_cr_clicked(self, button):
         #webbrowser.get('chromium').open_new_tab(sys.argv[1])
         process_one = subprocess.Popen(['google-chrome-stable', sys.argv[1]])
+        Gtk.main_quit()
+
+    def on_cr_clicked(self, button):
+        webbrowser.get('chromium').open_new_tab(sys.argv[1])
         Gtk.main_quit()
 
     def on_close_clicked(self, button):
