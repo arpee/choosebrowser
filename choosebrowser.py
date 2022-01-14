dc61955f902972c7efaead36dd779d21168c3cde
arpee <richard@saito.tech>
2022-01-14 10:06:49 +0800

    force gtk 3 and add two browsers

--- choosebrowser.py
+++ choosebrowser.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 import sys
 import webbrowser
 import subprocess
-from gi.repository import Gtk
+#import pyperclip
+#try:
+#        from Tkinter import Tk
+#except ImportError:
+#        from tkinter import Tk
+#        r = Tk()
+
 
 class ButtonWindow(Gtk.Window):
 
@@ -31,7 +40,19 @@ class ButtonWindow(Gtk.Window):
         hbox.pack_start(button, True, True, 0)
 
         button = Gtk.Button("Vivaldi")
-        button.connect("clicked", self.on_cr_clicked)
+        button.connect("clicked", self.on_vv_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button("Edge")
+        button.connect("clicked", self.on_edge_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button("Brave")
+        button.connect("clicked", self.on_brave_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button("Copy")
+        button.connect("clicked", self.on_cp_clicked)
         hbox.pack_start(button, True, True, 0)
 
         button = Gtk.Button("_Close", use_underline=True)
@@ -55,8 +76,25 @@ class ButtonWindow(Gtk.Window):
         webbrowser.get('chromium').open_new_tab(sys.argv[1])
         Gtk.main_quit()
 
-    def on_cr_clicked(self, button):
+    def on_vv_clicked(self, button):
         process_one = subprocess.Popen(['vivaldi-stable', sys.argv[1]])
+        Gtk.main_quit()
+
+    def on_edge_clicked(self, button):
+        process_one = subprocess.Popen(['microsoft-edge-dev', sys.argv[1]])
+        Gtk.main_quit()
+
+    def on_brave_clicked(self, button):
+        process_one = subprocess.Popen(['brave', sys.argv[1]])
+        Gtk.main_quit()
+
+    def on_cp_clicked(self, button):
+        #pyperclip.copy(sys.argv[1])
+        r.withdraw()
+        r.clipboard_clear()
+        r.clipboard_append('i can has clipboardz?')
+        r.update() # now it stays on the clipboard after the window is closed
+        #r.destroy()
         Gtk.main_quit()
 
     def on_close_clicked(self, button):
